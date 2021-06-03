86279ef7dcafb0600791c9a6d1832f197d015a0b
Mariano Scasso <mariano.scasso@scanoss.com>
2021-06-02 21:03:50 -0400

    corrected again

--- Formula/quickscan-lite.rb
+++ Formula/quickscan-lite.rb
@@ -1,7 +1,7 @@
 class QuickscanLite < Formula
   desc "Macos UI client for osskb.org"
   homepage "https://github.com/scanoss/quickscan"
-  url "(url omitted)"
+  url "https://github.com/scanoss/quickscan/releases/download/v1.2.4/quickscan-lite-macoss-1.2.4.tar.gz"
   sha256 "e04bb8bad27473c34cbfdbc091b1e043cef759736a74e82f54e5393e3a0fae6a"
 
   bottle :unneeded
